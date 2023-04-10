c63d82ecc838c71824182dc1817ee98eff7093ef
RennUW <91917951+RennUW@users.noreply.github.com>
2023-04-10 10:09:52 -0700

    Update question-2.sql

--- question-2.sql
+++ question-2.sql
@@ -1,1 +1,8 @@
 -- Show the sales agent's full name and invoices associated with each sales agent.
+SELECT e.FirstName || ' ' || e.LastName AS SalesAgentFullName, ii.InvoiceLineId, ii.TrackId, ii.UnitPrice, ii.Quantity
+FROM employees e
+JOIN customers c ON e.EmployeeId = c.SupportRepId
+JOIN invoices i ON c.CustomerId = i.CustomerId
+JOIN invoice_items ii ON i.InvoiceId = ii.InvoiceId
+WHERE e.Title = 'Sales Support Agent'
+ORDER BY SalesAgentFullName, ii.InvoiceLineId;
